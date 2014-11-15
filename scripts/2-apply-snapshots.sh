7309fbb289f9397add7076f00ae5c0f79857c40f
Bomfunk <mrbomfunk@gmail.com>
2014-11-15 12:35:02 +0300

    Minor fix

--- scripts/2-apply-snapshots.sh
+++ scripts/2-apply-snapshots.sh
@@ -2,10 +2,10 @@
 
 source env.cfg
 
-sudo rm -f fuel-pm.qcow2
+sudo rm -f diff.fuel-pm.qcow2
 qemu-img create -f qcow2 -b snapshots/fuel-pm.qcow2 diff.fuel-pm.qcow2
 for i in $(seq 1 $slaves_count)
 do
-	sudo rm -f fuel-slave-$i.qcow2
+	sudo rm -f diff.fuel-slave-$i.qcow2
 	qemu-img create -f qcow2 -b snapshots/fuel-slave-$i.qcow2 diff.fuel-slave-$i.qcow2
 done
