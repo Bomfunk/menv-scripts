3b5764281f326985fde5799bc021f97467f2c116
Bomfunk <mrbomfunk@gmail.com>
2015-02-13 17:18:11 +0300

    Little fix for the default preparation script

--- env-template/preparation/wait-for-master.sh
+++ env-template/preparation/wait-for-master.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source env.cfg
+source $PATH_TO_ENV/env.cfg
 
 COBBLER_LISTEN=0
 sleep 30
 echo "I will be trying to contact the master node through SSH every 15 seconds to see if it started listening to at least 3 ports in cobbler container. Please standby..."
 while [ $COBBLER_LISTEN -lt 3 ]
 do
-	COBBLER_LISTEN=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i master-key root@${subnet[1]}.2 dockerctl shell cobbler netstat -lntp | wc -l 2> /dev/null)
+	COBBLER_LISTEN=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $PATH_TO_ENV/master-key root@${subnet[1]}.2 dockerctl shell cobbler netstat -lntp | wc -l 2> /dev/null)
 	sleep 15
 done
