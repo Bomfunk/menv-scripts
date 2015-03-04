5490f7382f90b9f7e908141302da4fb44ed5e5b1
Bomfunk <mrbomfunk@gmail.com>
2015-03-04 12:17:16 +0300

    Fixed maintenance scripts

--- maintenance/3-stop-and-save.sh
+++ maintenance/3-stop-and-save.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <PATH_TO_ENV>"
+	echo "Where PATH_TO_ENV is path to the environment - snapshots, configuration file etc.,"
+	exit 1
+fi
+
 ./maintenance/save-snapshots.sh
 ./destroy-env.sh
 
