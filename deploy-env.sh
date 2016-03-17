382f0ef0ca4ba929f81da438924b2134bec15a2d
Mikhail Kraynov <kraynovmv@gmail.com>
2016-03-17 14:09:18 +0300

    Add ability to see network settings

--- deploy-env.sh
+++ deploy-env.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
 
-ALL_ARGS=$(getopt -o y --long yes -n $0 -- "$@")
+ALL_ARGS=$(getopt -o yi --long yes,info -n $0 -- "$@")
 eval set -- "$ALL_ARGS"
 
 NEEDCONFIRM=true
+NEEDINFO=false
 while true
 do
 	case "$1" in
 		-y|--yes) NEEDCONFIRM=false ; shift ;;
+		-i|--info) NEEDINFO=true ;shift;;
 		--) shift; break ;;
 		*) echo "Internal error." ; exit 1 ;;
 	esac
 done
+
+export PATH_TO_ENV=$(readlink -e $1)
+if [ ! -d "$PATH_TO_ENV" ]
+then
+	echo "The specified environment directory doesn't exist, aborting."
+	exit 1
+fi
+source $PATH_TO_ENV/env.cfg
+INET_IF=$2
+INET_IF_IP=$(ip -o -4 addr list $INET_IF | awk '{print $4}' | cut -d/ -f1)
+
+function show_net_info {
+        echo "Use IP address $master_ip to access Fuel Master, and $horizon_ip for Horizon."
+        if $external_forward
+        then
+                echo "Also, the following port forwards are set on this machine: "
+                echo "(Note: the IP of this host on $INET_IF is $INET_IF_IP)"
+                for i in $(seq 1 $forward_count)
+                do
+                        echo -n "0.0.0.0:${ex_forw[$i]} to ${ex_forw_to[$i]}"
+                        if [ $i -lt $forward_count ] ; then echo "," ; else echo "." ; fi
+                done
+        echo
+        else
+                echo "Port forwards were not configured on this machine."
+        fi
+                echo -n "If you use sshuttle, here is the suggested command for it: sshuttle -r mirantis@$INET_IF_IP "
+        for i in $(seq 1 $networks)
+        do
+                echo -n "${subnet[i]}.0/24 "
+        done
+        echo ; echo
+}
+
+if $NEEDINFO
+then
+	show_net_info
+	exit 1
+fi
 
 if [ $# -ne 2 ]
 then
@@ -20,6 +61,7 @@ then
 	echo "and INET_IF is a network interface name which has access to Internet (for Public network)."
 	echo "Options:"
 	echo " -y/--yes		Proceed without asking for a confirmation."
+	echo " -i/--info    Get the information about network settings"
 	exit 1
 fi
 
@@ -37,17 +79,6 @@ then
 		exit 1
 	fi
 fi
-
-export PATH_TO_ENV=$(readlink -e $1)
-if [ ! -d "$PATH_TO_ENV" ]
-then
-	echo "The specified environment directory doesn't exist, aborting."
-	exit 1
-fi
-
-INET_IF=$2
-INET_IF_IP=$(ip -o -4 addr list $INET_IF | awk '{print $4}' | cut -d/ -f1)
-source $PATH_TO_ENV/env.cfg
 
 if [ ! -e /sys/class/net/$INET_IF ]
 then
@@ -106,27 +137,8 @@ echo "Applying saved snapshots..."
 echo "Launching VMs..."
 ./scripts/3-launch-vms.sh
 
-echo "All done. Use IP address $master_ip to access Fuel Master, and $horizon_ip for Horizon."
-if $external_forward
-then
-	echo "Also, the following port forwards are set on this machine: "
-	echo "(Note: the IP of this host on $INET_IF is $INET_IF_IP)"
-	for i in $(seq 1 $forward_count)
-	do
-		echo -n "0.0.0.0:${ex_forw[$i]} to ${ex_forw_to[$i]}"
-		if [ $i -lt $forward_count ] ; then echo "," ; else echo "." ; fi
-	done
-	echo
-else
-	echo "Port forwards were not configured on this machine."
-fi
-
-echo -n "If you use sshuttle, here is the suggested command for it: sshuttle -r mirantis@$INET_IF_IP "
-for i in $(seq 1 $networks)
-do
-	echo -n "${subnet[i]}.0/24 "
-done
-echo ; echo # Double line break
+echo "All done."
+show_net_info
 
 echo "Use destroy-env.sh script to tear down the environment and destroy the networks."
 echo "Enjoy!"
